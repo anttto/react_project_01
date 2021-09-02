9b723253b09215c1c0dd0cfb696dcd488c8255c8
Antovero <dosaeun@gmail.com>
2021-09-02 16:23:12 +0900

    props

--- src/App.js
+++ src/App.js
@@ -69,6 +69,7 @@ function App() {
       <ul>
         <li>asdkjnasjkdnas</li>
         <li>asdkjnasjkdnas</li>
+        <li>asdkjnasjkdnas</li>
       </ul>
       {foodILike.map((item) => {
         return <Food key={item.id} name={item.name} src={item.image} rating={item.rating} />
