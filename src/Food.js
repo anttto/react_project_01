50b59ac687105feec9932a7478fb3d194aeaa073
Antovero <dosaeun@gmail.com>
2021-09-02 16:19:34 +0900

    props

---
+++ src/Food.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Food = () => {
+    return (
+        <div>
+            <h2>HAHAHA</h2>
+            <h2>HAHAHA</h2>
+            <h2>HAHAHA</h2>
+            <h2>HAHAHA</h2>
+            <h2>HAHAHA</h2>
+            <h2>HAHAHA</h2>
+        </div>
+    );
+};
+
+export default Food;
